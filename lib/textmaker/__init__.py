59086d6683214cd9f2d3af9f265ebc000edac281
ARH <65483182+arh-space@users.noreply.github.com>
2021-08-25 00:38:13 +0700

    Update __init__.py

--- lib/textmaker/__init__.py
+++ lib/textmaker/__init__.py
@@ -8,7 +8,7 @@ class tulis:
         self.text = listOrText
         self.output = []
     def tulis(self):
-        img, font, kata, tempkata=Image.open("lib/textmaker/image.png"), ImageFont.truetype("lib/textmaker/fontnya.ttf",70),'',''
+        img, font, kata, tempkata=Image.open("lib/textmaker/image.png"), ImageFont.truetype("lib/textmaker/fontnya.ttf",250),'',''
         draw=ImageDraw.Draw(img)
         if type(self.text) is not list:
             self.output=[]
@@ -22,10 +22,10 @@ class tulis:
             spliter=kata.split("\n")
         else:
             spliter=self.text
-        line=115
-        for i in spliter[:56]:
-            draw.text((70, int(line)), i, font=font, fill=("pink")) #selisih = Line
-            line+=85 + 4.0
+        line=215
+        for i in spliter[:70]:
+            draw.text((80, int(line)), i, font=font, fill=("pink")) #selisih = Line
+            line+=105 + 4.0
         self.output.append(img)
         if len(spliter) > 56:
             self.output+=tulis(spliter[56:]).tulis()
